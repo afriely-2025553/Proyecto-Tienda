beefbb22623c99f9cbd4fb8d4871b4258be66e73
afriely-2025553 <afriely-2025553@kinal.edu.gt>
2026-04-15 11:48:21 -0600

    feat: Se agrego el CRUD de usuarios

--- Ejemplo/src/main/java/com/example/Ejemplo/Controller/UsuariosController.java
+++ Ejemplo/src/main/java/com/example/Ejemplo/Controller/UsuariosController.java
@@ -5,12 +5,16 @@ import com.example.Ejemplo.Service.UsuariosService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/usuarios")
+@Controller
+@RequestMapping("/usuarios")
 public class UsuariosController {
 
     private final UsuariosService usuariosService;
@@ -20,47 +24,59 @@ public class UsuariosController {
     }
 
     @GetMapping
-    public List<Usuarios> getAllUsuarios() {
-        return usuariosService.getAllUsuarios();
+    public String listar(Model model) {
+        List<Usuarios> lista = usuariosService.getAllUsuarios();
+        model.addAttribute("usuarios", lista);
+        return "usuarios";
     }
 
-    @PostMapping
-    public ResponseEntity<Object> createUsuarios(@Valid @RequestBody Usuarios usuarios) {
-        try {
-            Usuarios createUsuarios = usuariosService.saveUsuarios(usuarios);
-            return new ResponseEntity<>(createUsuarios, HttpStatus.CREATED);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+
+    @GetMapping("/register")
+    public String registro() {
+        return "register";
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Object> updateUsuarios(@PathVariable Integer id, @Valid @RequestBody Usuarios usuarios) {
-        try {
-            Usuarios updateUsuarios = usuariosService.updateUsuarios(id, usuarios);
-            return new ResponseEntity<>(updateUsuarios, HttpStatus.OK);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+    @PostMapping("/registro")
+    public String guardar(@RequestParam("username") String username,
+                          @RequestParam("password") String password,
+                          @RequestParam("email") String email,
+                          Model model) {
+
+        Usuarios u = usuariosService.registrar(username, password, email);
+
+        if (u == null) {
+            model.addAttribute("error", "Usuario ya existe");
+            return "register";
         }
+
+        return "redirect:/acceder";
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Object> getUsuariosById(@PathVariable Integer id) {
-        try {
-            Usuarios searchUsuarios = usuariosService.getUsuariosById(id);
-            return new ResponseEntity<>(searchUsuarios, HttpStatus.OK);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+    @GetMapping("/editar/{id}")
+    public String editar(@PathVariable Integer id, Model model) {
+        Usuarios usuario = usuariosService.getUsuariosById(id);
+
+        if (usuario == null) {
+            return "redirect:/usuarios"; // evita error si no existe
         }
+
+        model.addAttribute("usuario", usuario);
+        return "editar_usuario";
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deleteUsuarios(@PathVariable Integer id) {
-        try {
-            usuariosService.deleteUsuarios(id);
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+//    @GetMapping("/{id}")
+//    public ResponseEntity<Object> getUsuariosById(@PathVariable Integer id) {
+//        try {
+//            Usuarios searchUsuarios = usuariosService.getUsuariosById(id);
+//            return new ResponseEntity<>(searchUsuarios, HttpStatus.OK);
+//        } catch (IllegalArgumentException e) {
+//            return ResponseEntity.badRequest().body(e.getMessage());
+//        }
+//    }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminarUsuario(@PathVariable Integer id) {
+        usuariosService.deleteUsuarios(id);
+        return "redirect:/usuarios";
     }
 }
