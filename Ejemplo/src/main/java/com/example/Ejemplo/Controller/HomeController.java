d0de23554aeda075b75fc546b8fff85244b4124b
afrily-2025553 <afriely-2025553@kinal.edu.gt>
2026-04-15 23:23:28 -0600

    feat: se agregaron cacmbios en HomeController

--- Ejemplo/src/main/java/com/example/Ejemplo/Controller/HomeController.java
+++ Ejemplo/src/main/java/com/example/Ejemplo/Controller/HomeController.java
@@ -1,38 +1,49 @@
 package com.example.Ejemplo.Controller;
 
+import com.example.Ejemplo.Entity.Usuarios;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/home")
-    public String home() {
+    public String home(HttpSession session, Model model) {
+
+        Usuarios usuario = (Usuarios) session.getAttribute("usuarioLogueado");
+
+        if (usuario == null) {
+            return "redirect:/acceder";
+        }
+
+        model.addAttribute("usuario", usuario);
+
         return "home";
     }
 
-    @GetMapping("/clientes")
-    public String clientes() {
-        return "clientes";
-    }
-
     @GetMapping("/detalleVenta")
-    public String detalleVenta() {
+    public String detalleVenta(HttpSession session) {
+        if (session.getAttribute("usuarioLogueado") == null) {
+            return "redirect:/acceder";
+        }
         return "detalleVenta";
     }
 
     @GetMapping("/productos")
-    public String productos() {
+    public String productos(HttpSession session) {
+        if (session.getAttribute("usuarioLogueado") == null) {
+            return "redirect:/acceder";
+        }
         return "productos";
     }
 
-    @GetMapping("/usuarios-view")
-    public String usuarios() {
-        return "usuarios";
-    }
-
     @GetMapping("/ventas")
-    public String ventas() {
+    public String ventas(HttpSession session) {
+        if (session.getAttribute("usuarioLogueado") == null) {
+            return "redirect:/acceder";
+        }
         return "ventas";
     }
 }
