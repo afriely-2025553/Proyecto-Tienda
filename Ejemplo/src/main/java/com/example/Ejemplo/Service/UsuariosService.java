6b31bcda87565c400fff1ccea85f608c4c57ff6f
afrily-2025553 <afriely-2025553@kinal.edu.gt>
2026-04-15 23:42:36 -0600

    feat: se agregaron metodos a UsuariosService

--- Ejemplo/src/main/java/com/example/Ejemplo/Service/UsuariosService.java
+++ Ejemplo/src/main/java/com/example/Ejemplo/Service/UsuariosService.java
@@ -11,7 +11,7 @@ public interface UsuariosService {
     Usuarios getUsuariosById(Integer id);
     Usuarios saveUsuarios(Usuarios usuarios) throws RuntimeException;
     Usuarios login(String usuarios, String password);
-    Usuarios registrar(String usuario, String password, String email);
+    Usuarios registrar(String usuario, String password, String email, String rol);
     Usuarios updateUsuarios(Integer id, Usuarios usuarios);
     void deleteUsuarios(Integer id);
 }
